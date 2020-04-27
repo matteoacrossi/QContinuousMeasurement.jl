2fd39aa9be8dd9f56b1b61712e7f7cff5d6af497
Matteo Rossi <teo.red90@gmail.com>
2020-04-27 14:00:07 +0300

    Fixed exports

--- src/Eff_QFI_HD.jl
+++ src/Eff_QFI_HD.jl
@@ -6,10 +6,67 @@ function Unconditional_QFI(Nj::Int64, Tfinal::Real, dt::Real;
     κ::Real = 1.,                    # Independent noise strength
     κcoll::Real = 1.,                # Collective noise strength
     ω::Real = 0.0                   # Frequency of the Hamiltonian
-    )            
+    )
     return Eff_QFI_HD(Nj, 1, Tfinal, dt; κ=κ, κcoll = κcoll, ω=ω, η=0.0)
 end
 
+
+"""
+(t, FI, QFI) = Eff_QFI_HD(Nj, Ntraj, Tfinal, dt; kwargs... )
+
+Evaluate the continuous-time FI and QFI of a final strong measurement for the
+estimation of the frequency ω with continuous homodyne monitoring of
+collective transverse noise, with each half-spin particle affected by
+parallel noise, with efficiency η using SME
+(stochastic master equation).
+
+The function returns a NamedTuple `(t, FI, QFI)` containing the time vector and the vectors containing the FI and average QFI
+
+# Arguments
+
+* `Nj`: number of spins
+* `Ntraj`: number of trajectories for the SSE
+* `Tfinal`: final time of evolution
+* `dt`: timestep of the evolution
+* `κ = 1`: the independent noise coupling
+* `κcoll = 1`: the collective noise coupling
+* `ω = 0`: local value of the frequency
+* `η = 1`: measurement efficiency
+"""
+function Eff_QFI_HD(
+                    Nj::Int64, # Number of spins
+                    Ntraj::Int64,                    # Number of trajectories
+                    Tfinal::Real,                    # Final time
+                    dt::Real;                        # Time step
+                    κ::Real = 1.,                    # Independent noise strength
+                    κcoll::Real = 1.,                # Collective noise strength
+                    ω::Real = 0.0,                   # Frequency of the Hamiltonian
+                    η::Real = 1.
+                    )
+
+    θ = 0
+    # ω is the parameter that we want to estimate
+    H = ω * σ(:z, Nj) / 2       # Hamiltonian of the spin system
+
+    dH = σ(:z, Nj) / 2          # Derivative of H wrt the parameter ω
+
+    #non_monitored_noise_op = []
+    non_monitored_noise_op = (sqrt(κ/2) *
+    [(cos(θ) * σ_j(:z, j, Nj) + sin(θ) * σ_j(:y, j, Nj)) for j = 1:Nj])
+
+    monitored_noise_op = [sqrt(κcoll) * σ(:y, Nj)/2]
+
+    res = Eff_QFI_HD(Ntraj,# Number of trajectories
+    Tfinal,                           # Final time
+    dt,                               # Time step
+    H, dH,                            # Hamiltonian and its derivative wrt ω
+    non_monitored_noise_op,           # Non monitored noise operators
+    monitored_noise_op;               # Monitored noise operators
+    initial_state = coherent_state,
+    η=η)
+
+    return res
+end
 
 """
     (t, FI, QFI) = Eff_QFI_HD(Nj, Ntraj, Tfinal, dt; kwargs... )
@@ -71,7 +128,7 @@ function Eff_QFI_HD(Ntraj::Int64,       # Number of trajectories
     # Kraus-like operator, trajectory-independent part
 
     M0 = sparse(I - 1im * H * dt -
-                0.5 * dt * sum([c' * c for c in cj]) - 
+                0.5 * dt * sum([c' * c for c in cj]) -
                 0.5 * dt * sum([c' * c for c in Cj]))
 
     # Derivative of the Kraus-like operator wrt to ω
@@ -159,16 +216,16 @@ function Eff_QFI_HD(Ntraj::Int64,       # Number of trajectories
         hcat(FisherT, QFisherT, jx, jy, jz, jx2, jy2, jz2)
     end
 
-    jx=result[:,3] / Ntraj 
-    jy=result[:,4] / Ntraj 
+    jx=result[:,3] / Ntraj
+    jy=result[:,4] / Ntraj
     jz=result[:, 5] / Ntraj
     Δjx=result[:,6] / Ntraj - jx.^2
     Δjy=result[:,7] / Ntraj - jy.^2
     Δjz=result[:,8] / Ntraj - jz.^2
 
-    return (t=t, 
-            FI=result[:,1] / Ntraj, 
-            QFI=result[:,2] / Ntraj, 
+    return (t=t,
+            FI=result[:,1] / Ntraj,
+            QFI=result[:,2] / Ntraj,
             jx=jx, jy=jy, jz=jz,
             Δjx=Δjx, Δjy=Δjy, Δjz=Δjz)
 end
--- src/QContinuousMeasurement.jl
+++ src/QContinuousMeasurement.jl
@@ -33,7 +33,10 @@ module QContinuousMeasurement
     export Eff_QFI_HD_Dicke, Eff_QFI_HD_Dicke_0
     export simulate_trajectory
     export liouvillian, jspin, css
-    export Model, ModelParameters, State, updatestate!, measure_current, expectation_value!
+    export Model, ModelParameters, State
+    export CollectiveDephasingModel, CollectiveDephasingModelParameters, FixedjState
+    export CollectiveLocalDephasingModel, CollectiveLocalDephasingModelParameters, BlockDiagonalState
+    export updatestate!, measure_current, expectation_value!
     export get_time
     export InitializeModel, blockdiag_css
     export FileWriter
@@ -52,60 +55,4 @@ module QContinuousMeasurement
     include("utils.jl")
     include("show.jl") # Pretty formatting
 
-    """
-        (t, FI, QFI) = Eff_QFI_HD(Nj, Ntraj, Tfinal, dt; kwargs... )
-
-    Evaluate the continuous-time FI and QFI of a final strong measurement for the
-    estimation of the frequency ω with continuous homodyne monitoring of
-    collective transverse noise, with each half-spin particle affected by
-    parallel noise, with efficiency η using SME
-    (stochastic master equation).
-
-    The function returns a NamedTuple `(t, FI, QFI)` containing the time vector and the vectors containing the FI and average QFI
-
-    # Arguments
-
-    * `Nj`: number of spins
-    * `Ntraj`: number of trajectories for the SSE
-    * `Tfinal`: final time of evolution
-    * `dt`: timestep of the evolution
-    * `κ = 1`: the independent noise coupling
-    * `κcoll = 1`: the collective noise coupling
-    * `ω = 0`: local value of the frequency
-    * `η = 1`: measurement efficiency
-    """
-    function Eff_QFI_HD(
-        Nj::Int64, # Number of spins
-        Ntraj::Int64,                    # Number of trajectories
-        Tfinal::Real,                    # Final time
-        dt::Real;                        # Time step
-        κ::Real = 1.,                    # Independent noise strength
-        κcoll::Real = 1.,                # Collective noise strength
-        ω::Real = 0.0,                   # Frequency of the Hamiltonian
-        η::Real = 1.
-    )
-
-    θ = 0
-    # ω is the parameter that we want to estimate
-    H = ω * σ(:z, Nj) / 2       # Hamiltonian of the spin system
-
-    dH = σ(:z, Nj) / 2          # Derivative of H wrt the parameter ω
-
-    #non_monitored_noise_op = []
-    non_monitored_noise_op = (sqrt(κ/2) *
-        [(cos(θ) * σ_j(:z, j, Nj) + sin(θ) * σ_j(:y, j, Nj)) for j = 1:Nj])
-
-    monitored_noise_op = [sqrt(κcoll) * σ(:y, Nj)/2]
-
-    res = Eff_QFI_HD(Ntraj,# Number of trajectories
-        Tfinal,                           # Final time
-        dt,                               # Time step
-        H, dH,                            # Hamiltonian and its derivative wrt ω
-        non_monitored_noise_op,           # Non monitored noise operators
-        monitored_noise_op;               # Monitored noise operators
-        initial_state = coherent_state,
-        η=η)
-
-    return res
-    end
 end
