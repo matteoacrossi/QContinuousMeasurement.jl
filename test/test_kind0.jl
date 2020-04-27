a664ebddf758210914eb8ba82e7af62f336e2905
Matteo Rossi <teo.red90@gmail.com>
2020-04-27 13:22:13 +0300

    Add test for CollectiveDephasingModel

---
+++ test/test_kind0.jl
@@ -0,0 +1,25 @@
+using QContinuousMeasurement
+using Test
+using Random
+
+@testset "kind=0" begin
+    params = QContinuousMeasurement.CollectiveLocalDephasingModelParameters(Nj = 2, kind = 0)
+    model = QContinuousMeasurement.CollectiveLocalDephasingModel(params)
+    state = QContinuousMeasurement.blockdiag_css(params.Nj)
+
+    params2 = QContinuousMeasurement.CollectiveDephasingModelParameters(Nj = 2)
+    model2 = QContinuousMeasurement.CollectiveDephasingModel(params2)
+    state2 = QContinuousMeasurement.fixedj_css(params2.Nj)
+
+    seed = 1
+
+    Random.seed!(seed)
+    res = simulate_trajectory(model, state)
+
+    Random.seed!(seed)
+    res2 = simulate_trajectory(model2, state2)
+
+    for k in keys(res)
+        @test getfield(res, k) ≈ getfield(res2, k)
+    end
+end
