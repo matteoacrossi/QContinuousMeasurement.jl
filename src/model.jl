ab4cf2f92d6dbb27a1a79d7c8c4dc81da4ccc8db
Matteo Rossi <teo.red90@gmail.com>
2020-04-27 13:20:46 +0300

    Specialized models

--- src/model.jl
+++ src/model.jl
@@ -3,7 +3,11 @@ using SparseArrays
 using LinearAlgebra
 using TimerOutputs
 
-struct ModelParameters
+abstract type Model end
+
+abstract type ModelParameters end
+
+struct CollectiveLocalDephasingModelParameters <: ModelParameters
     Nj::Integer
     kind::Real
     kcoll::Real
@@ -15,7 +19,7 @@ struct ModelParameters
     outpoints::Integer
     _outsteps::Integer
 
-    function ModelParameters(; Nj::Integer=1,
+    function CollectiveLocalDephasingModelParameters(; Nj::Integer=1,
                                kind::Real=1.0,
                                kcoll::Real=1.0,
                                omega::Real=1.0,
@@ -47,32 +51,13 @@ function get_time(mp::ModelParameters)
     t[mp._outsteps:mp._outsteps:end]
 end
 
-struct State
-    ρ::BlockDiagonal
-    dρ::BlockDiagonal
-    τ::BlockDiagonal
-    # Internal fields
-    _tmp1::BlockDiagonal
-    _tmp2::BlockDiagonal
-    _new_ρ::BlockDiagonal
-
-    State(ρ::BlockDiagonal) = new(ρ, zero(ρ), zero(ρ), similar(ρ), similar(ρ), similar(ρ))
-end
-# Copy constructor
-State(state::State) = State(copy(state.ρ))
-
-function coherentspinstate(Nj, dense=true)
-    ρ0 = blockdiagonal(css(Nj), dense=dense)
-    State(ρ0)
-end
-
 struct KrausOperator
     M0::BlockDiagonal
     M::BlockDiagonal
 end
 
-struct Model
-    params::ModelParameters
+struct CollectiveLocalDephasingModel <: Model
+    params::CollectiveLocalDephasingModelParameters
     Jx::BlockDiagonal
     Jy::BlockDiagonal
     Jz::BlockDiagonal
@@ -83,7 +68,7 @@ struct Model
     M0::BlockDiagonal
     dM::BlockDiagonal
 
-    function Model(modelparams::ModelParameters, liouvillianfile::Union{String, Nothing}=nothing)
+    function CollectiveLocalDephasingModel(modelparams::CollectiveLocalDephasingModelParameters, liouvillianfile::Union{String, Nothing}=nothing)
         Nj = modelparams.Nj
         dt = modelparams.dt
         kcoll = modelparams.kcoll
@@ -141,7 +126,7 @@ function initliouvillian(Nj::Integer, filename::String)
     return liouvillian + Nj*I
 end
 
-function measure_current(state::State, model::Model)
+function measure_current(state::BlockDiagonalState, model::CollectiveLocalDephasingModel)
     @inline dW() = sqrt(model.params.dt) * randn() # Define the Wiener increment
     # Homodyne current (Eq. 35)
     # dy = 2 sqrt(kcoll * eta) * tr(ρ * Jy) * dt + dW
@@ -149,7 +134,7 @@ function measure_current(state::State, model::Model)
     return 2 * sqrt(model.params.kcoll * model.params.eta) * real(tr(state._tmp1)) * model.params.dt + dW()
 end
 
-function get_kraus(model::Model, dy::Real)
+function get_kraus(model::CollectiveLocalDephasingModel, dy::Real)
     p = model.params
     kraus = similar(model.M0)
     # Kraus operator Eq. (36)
@@ -160,7 +145,7 @@ function get_kraus(model::Model, dy::Real)
     return kraus
 end
 
-function updatestate!(state::State, model::Model, dy::Real)
+function updatestate!(state::BlockDiagonalState, model::CollectiveLocalDephasingModel, dy::Real)
     # Get Kraus operator for measured current dy
     @timeit_debug "get_kraus" M = get_kraus(model, dy)
 
@@ -213,4 +198,158 @@ function updatestate!(state::State, model::Model, dy::Real)
     return tr_ρ, tr_τ
 end
 
-expectation_value!(state::State, op::AbstractArray) = real(tr(mul!(state._tmp1, op, state.ρ)))
+expectation_value!(state::State, op::AbstractArray) = real(tr(mul!(state._tmp1, op, density_matrix(state))))
+
+
+struct CollectiveDephasingModelParameters <: ModelParameters
+    Nj::Integer
+    kcoll::Real
+    kind::Real
+    omega::Real
+    eta::Real
+    dt::Real
+    Tfinal::Real
+    Ntime::Integer
+    outpoints::Integer
+    _outsteps::Integer
+
+    function CollectiveDephasingModelParameters(; Nj::Integer=1,
+                               kcoll::Real=1.0,
+                               omega::Real=1.0,
+                               eta::Real=1.0,
+                               dt::Real=0.0001,
+                               Tfinal::Real=1.0,
+                               outpoints::Integer=0)
+
+        Ntime = Int(floor(Tfinal/dt)) # Number of timesteps
+
+        outsteps = 1
+        if outpoints > 0
+
+            try
+                outsteps = Int(round(Tfinal / dt / outpoints, digits=3))
+            catch InexactError
+                @warn "The requested $outpoints output points does not divide
+                the total time steps. Using the full time output."
+            end
+        end
+        #outpoints = Ntime
+
+        new(Nj, kcoll, 0.0, omega, eta, dt, Tfinal, Ntime, outpoints, outsteps)
+    end
+end
+
+struct CollectiveDephasingModel <: Model
+    params::CollectiveDephasingModelParameters
+    Jx::SparseMatrixCSC
+    Jy::SparseMatrixCSC
+    Jz::SparseMatrixCSC
+    Jx2::SparseMatrixCSC
+    Jy2::SparseMatrixCSC
+    Jz2::SparseMatrixCSC
+    second_term::SparseMatrixCSC
+    M0::SparseMatrixCSC
+    dM::SparseMatrixCSC
+
+    function CollectiveDephasingModel(modelparams::CollectiveDephasingModelParameters, liouvillianfile::Union{String, Nothing}=nothing)
+        Nj = modelparams.Nj
+        dt = modelparams.dt
+        kcoll = modelparams.kcoll
+        ω = modelparams.omega
+        η = modelparams.eta
+
+        firstblock = block_sizes(Nj)[1]
+        # Spin operators (only the first block)
+        (Jx, Jy, Jz) = map(x -> sparse(x[1:firstblock,1:firstblock]), jspin(Nj))
+
+        # TOODO: Find better name
+        second_term = sqrt((1 - η) * dt * kcoll) * Jy
+
+        Jx2 = Jx^2
+        Jy2 = Jy^2
+        Jz2 = Jz^2
+
+        H = ω * Jz
+        dH = Jz
+
+        # Kraus-like operator, trajectory-independent part
+        M0 = I - 1im * H * dt - (kcoll/2) * Jy2 * dt
+
+        # Derivative of the Kraus-like operator wrt to ω
+        dM = -1im * dH * dt
+
+        new(modelparams, Jx, Jy, Jz, Jx2, Jy2, Jz2, second_term, M0, dM)
+    end
+end
+
+function measure_current(state::FixedjState, model::CollectiveDephasingModel)
+    @inline dW() = sqrt(model.params.dt) * randn() # Define the Wiener increment
+    # Homodyne current (Eq. 35)
+    # dy = 2 sqrt(kcoll * eta) * tr(ρ * Jy) * dt + dW
+    mul!(state._tmp1, model.Jy, state.ρ)
+    return 2 * sqrt(model.params.kcoll * model.params.eta) * real(tr(state._tmp1)) * model.params.dt + dW()
+end
+
+function get_kraus(model::CollectiveDephasingModel, dy::Real)
+    p = model.params
+    kraus = similar(model.M0)
+
+    # Kraus operator Eq. (36)
+    kraus = (model.M0 + sqrt(p.eta * p.kcoll) * model.Jy * dy +
+            p.eta * (p.kcoll / 2) * model.Jy2 * (dy^2 - p.dt))
+
+    return kraus
+end
+
+function updatestate!(state::FixedjState, model::CollectiveDephasingModel, dy::Real)
+    # Get Kraus operator for measured current dy
+    @timeit_debug "get_kraus" M = get_kraus(model, dy)
+
+    # Non-allocating code for
+    # new_ρ = Mpre * Mpost * ρ + second_term * ρ
+
+    @timeit_debug "rho" begin
+        mul!(state._tmp1, state.ρ, M')
+        mul!(state._new_ρ, M, state._tmp1)
+
+        # Apply the second term of Eq. (34)
+        mul!(state._tmp1, state.ρ, model.second_term')
+        mul!(state._new_ρ, model.second_term, state._tmp1, 1., 1.)
+
+        zchop!(state._new_ρ) # Round off elements smaller than 1e-14
+        tr_ρ = tr(state._new_ρ)
+        # Evolve the unnormalized derivative wrt ω
+    end
+    # Non-allocating code for
+    # τ = (Mpre * (Mpost * τ  +  dMpost * ρ) + dMpre * Mpost * ρ +
+    #      second_term * τ )/ tr_ρ;
+    @timeit_debug "tau" begin
+        # (Mpost * τ  +  dMpost * ρ)
+        mul!(state._tmp1, state.ρ, model.dM')
+        mul!(state._tmp1, state.τ, M', 1., 1.)
+
+        # second_term * τ
+        mul!(state._tmp2, model.second_term, state.τ)
+        mul!(state.τ, state._tmp2, model.second_term')
+
+        # (Mpre * (Mpost * τ  +  dMpost * ρ) + second_term * τ )
+        mul!(state.τ, M, state._tmp1, 1., 1.)
+
+        #  ... + dMpre * Mpost * ρ
+        mul!(state._tmp1, state.ρ, M')
+        mul!(state.τ, model.dM, state._tmp1, 1., 1.)
+
+        state.τ ./=  tr_ρ
+    end
+
+    zchop!(state.τ) # Round off elements smaller than 1e-14
+
+    tr_τ = tr(state.τ)
+    # Now we can renormalize ρ and its derivative wrt ω
+    # TODO: Use broadcasting when it is implemented
+    @timeit_debug "norms" begin
+        state.ρ  .= state._new_ρ ./ tr_ρ
+        state.dρ .= state.τ .- tr_τ .* state.ρ
+    end
+    return tr_ρ, tr_τ
+end
