aaefedc733ed32f622eb9092a040ca8bd58f163b
Matteo Rossi <teo.red90@gmail.com>
2020-08-26 19:06:20 +0300

    Removed useless functions

--- src/States.jl
+++ src/States.jl
@@ -65,22 +65,3 @@ function coherent_state(n::Int)
     end
     return kron([(spinup + spindown) / sqrt(2.) for i in 1:n]...)
 end
-
-"""
-    ghz_state(n)
-
-Return the GHZ state ``(\\ket{00...0} + \\ket{11...1}) / \\sqrt{2}`` in the computational basis
-"""
-function ghz_state(n::Int)
-    @assert n > 0 "n must be a positive integer"
-    r = zeros(Complex{Float64}, 2^n)
-    r[1] = r[end] = 1. / sqrt(2.)
-    return r
-end
-
-function random_state(n::Int)
-    @assert n > 0 "n must be a positive integer"
-    r = rand(Complex{Float64}, 2^n)
-    r /= norm(r)
-    return r
-end
