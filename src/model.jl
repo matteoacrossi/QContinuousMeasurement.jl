3283470238030425f05872e48c12655b4058af6f
Qtech admin <matteo.rossi@unimi.it>
2021-08-06 10:44:43 +0200

    Fix

--- src/model.jl
+++ src/model.jl
@@ -27,7 +27,7 @@ struct ModelParameters
                                eta::Real=1.0,
                                dt::Real=0.0001,
                                Tfinal::Real=1.0,
-                               outpoints::Integer=0
+                               outpoints::Integer=0,
                                mu::Real=0.0)
 
         Ntime = Int(floor(Tfinal / dt)) # Number of timesteps
@@ -44,7 +44,7 @@ struct ModelParameters
         end
         # outpoints = Ntime
 
-        new(Nj, kind, kcoll, Gamma, omega, eta, dt, Tfinal, Ntime, outpoints, outsteps, mu)
+        new(Nj, kind, kcoll, Gamma, omega, eta, dt, Tfinal, Ntime, outpoints, mu, outsteps)
     end
 end
 
